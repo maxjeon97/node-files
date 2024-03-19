80908127dfcb335dd6af11faac1820c6c36a277f
Max Jeon <maxjeon97@gmail.com>
2024-03-19 12:26:51 -0700

    Finish step 1 and step 2

--- step1.js
+++ step1.js
@@ -4,15 +4,21 @@ const fsP = require('fs/promises');
 
 const argv = process.argv;
 
+/**Given a path, attempts to read file at path. If successful, prints contents
+ * to the console, else, prints error message to console
+ */
 async function cat(path) {
+  let contents;
+
   try {
-    let contents = await fsP.readFile(path, "utf8");
-    console.log(contents);
+    contents = await fsP.readFile(path, "utf8");
   }
   catch (err) {
     console.error(`Error reading ${path}: ${err}`);
     process.exit(1);
   }
+
+  console.log(contents);
 }
 
 cat(argv[2]);
--- step2.js
+++ step2.js
@@ -1,35 +1,45 @@
 "use strict";
 
-const isUrl = require('is-url');
-
 const fsP = require('fs/promises');
 
 const argv = process.argv;
 
+/**Given a path, attempts to read file at path. If successful, prints contents
+ * to the console, else, prints error message to console
+ */
 async function cat(path) {
+  let contents;
+
   try {
-    let contents = await fsP.readFile(path, "utf8");
-    console.log(contents);
+    contents = await fsP.readFile(path, "utf8");
   }
   catch (err) {
     console.error(`Error reading ${path}: ${err}`);
     process.exit(1);
   }
+
+  console.log(contents);
 }
 
+/**Given a url, attempts to read text content at url. If successful, prints text
+ * to the console, else, prints error message to console
+ */
 async function webCat(url) {
+  let response;
+
   try {
-    let response = await fetch(url);
-    console.log(await response.text());
+    response = await fetch(url);
   }
   catch (err) {
     console.error(`Error fetching ${url}: ${err}`);
     process.exit(1);
   }
+
+  console.log(await response.text());
 }
 
 const path = argv[2];
 
-isUrl(path)
+URL.canParse(path)
 ? webCat(path)
 : cat(path);
